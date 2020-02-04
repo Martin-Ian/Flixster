2203add1cf75604dc96b8b09397096a3a1c76101
Ian Martin <martinianthecoder@gmail.com>
2020-02-03 20:16:19 -0800

    Added JSON parsing

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -7,6 +7,13 @@ import android.util.Log;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixster.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 import okhttp3.Headers;
 
@@ -15,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     public static final String TAG = "MainActivity";
 
+    List<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +35,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
                 Log.d(TAG, "Success");
+                //Create a JSONObject to hold the data we got from the server
+                JSONObject jsonObject = json.jsonObject;
+                //Try to parse through it.
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.e(TAG, "Results: " + results.toString());
+
+                    //Calling our Movie.java class to handle the Parsing logic.
+                    movies = Movie.fromJsonArray(results);
+                } catch (JSONException e) {
+                    //Key doesn't exist or other issue...
+                    Log.e(TAG, "Oops.. JSON had a problem");
+                    e.printStackTrace();
+                }
             }
 
             @Override
